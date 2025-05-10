6d707d6dd32a7c52b647e3660ad7b021d1fafde0
Ctrl+S <88386746+cherrychenhuan@users.noreply.github.com>
2025-05-10 19:19:48 +0800

    Add files via upload

---
+++ src/test/java/com/agric/myagric/Areas.java
@@ -0,0 +1,57 @@
+package com.sample;
+
+
+public class Areas {
+
+  private String code;
+  private String name;
+  private long level;
+  private String pcode;
+  private String category;
+
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public long getLevel() {
+    return level;
+  }
+
+  public void setLevel(long level) {
+    this.level = level;
+  }
+
+
+  public String getPcode() {
+    return pcode;
+  }
+
+  public void setPcode(String pcode) {
+    this.pcode = pcode;
+  }
+
+
+  public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+}
